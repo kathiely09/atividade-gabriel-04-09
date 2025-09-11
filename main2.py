805d3fa64c8bff1f78d945eb557746d9a4b1346b
Carla <santos.souza.carla@escola.pr.gov.br>
2025-09-11 12:06:35 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,40 +1,31 @@
 import pygame
 import os
-
-#inicializando o Pygame 
+#Inicializando o Pygame
 pygame.init()
-
-#definindo o tamanho da janela 
-WIDTH, HEIGHT=800, 600
-screen=pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption(" Janela com Imagem") 
-
-# Definindo a cor do fundo
-BG_COLOR = (30, 30, 40)
-#loop principal do jogo  # cor de fundo (um tom escuro)
-
-# Carregar a imagem
-imagem_file = "player.png" # coloque o nome da sua imagem
-if os.path.exists(imagem_file):
-    Img = pygame.image.load(image_file).convert_alpha()
-    Img_rect = img.get_rect(center=(WIDTH// 2, HEIGHT// 2 )) #centraliza a imagem
+#Definindo o tamanho da janela
+WIDTH, HEIGHT = 1000, 720
+screen pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Janela com Imagem")
+#Definindo a cor de fundo
+BG_COLOR (30, 30, 40) #cor de fundo (um tom escuro)
+#Carregar a imagem
+image_file = "GAME\player.png" coloque o nome da sua imagem aqui
+if os.path.exists(image_file):
+# Carregar a imagen img pygame.image.load(image_file).convert_alpha()
+img_rect img.get_rect(center (WIDTH // 2, HEIGHT // 2)) Centraliza a imagem
 else:
-    print("Imagem Não encontrada !")
-
-#loop principal do jogo 
-running= True
+print("Imagem não encontrada!")
+#Loop principal do jogo
+running True
 while running:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            running=False
-
-# Preencher o fundo 
-  screen.fill(BG_COLOR)
-
-# Desenhar a imagem da tela
-  screen.blit
-    #Atualizar a tela
-    pygame.display.flip()
-
-# Finalizar o Pygame
+for event in pygame.event.get():
+if event.type pygame.QUIT:
+running False
+#Preencher o fundo
+screen.fill (BG_COLOR)
+# Desenhar a imagem na tela
+screen.blit(img, img rect.topleft)
+#Atualizar a tela
+pygame.display.flip()
+#Finalizar o Pygame
 pygame.quit()
